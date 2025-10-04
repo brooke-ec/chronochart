2caf2b99c1856a3c1e4bbddb89fd87908b0ed7a7
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-10-04 16:54:18 +0100

    feat: redesign homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,12 @@
 	<img src="/logo.svg" alt="Chronochart Logo" draggable="false" />
 	<h1>chronochart</h1>
 	<div class="controls">
-		<button class="button" onclick={newProject}>
-			<Fa icon={faFileCirclePlus} size="2x" />
+		<button class="link" onclick={newProject}>
+			<Fa icon={faFileCirclePlus} />
 			New Project
 		</button>
-		<button class="button" onclick={openProject}>
-			<Fa icon={faFolderOpen} size="2x" />
+		<button class="link" onclick={openProject}>
+			<Fa icon={faFolderOpen} />
 			Open Project
 		</button>
 	</div>
@@ -26,7 +26,7 @@
 		justify-content: center;
 		flex-direction: column;
 		align-items: center;
-		padding-top: 25px;
+		padding-top: max(25px, 10vh);
 		user-select: none;
 		display: flex;
 		flex-grow: 1;
@@ -39,14 +39,15 @@
 
 	.controls {
 		grid-template-columns: repeat(2, 1fr);
-		margin-top: 60px;
+		margin-top: 10px;
 		display: grid;
-		gap: 20px;
+		gap: 10px 10vw;
 
 		button {
-			flex-direction: column;
+			flex-direction: row;
 			align-items: center;
 			display: flex;
+			gap: 10px;
 		}
 	}
 
