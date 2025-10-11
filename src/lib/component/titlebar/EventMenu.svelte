e944e1bd467a3f564c1b16e82061342c406022cf
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-10-11 23:50:31 +0100

    fix: timeline and event menu available at title

--- src/lib/component/titlebar/EventMenu.svelte
+++ src/lib/component/titlebar/EventMenu.svelte
@@ -2,6 +2,7 @@
 	import { open as openDialog } from "$lib/component/timeline/event/EventDialog.svelte";
 	import type { MenuBuilder } from "./Titlebar.svelte";
 	import { melt } from "@melt-ui/svelte";
+	import { page } from "$app/state";
 
 	interface Props {
 		createMenu: MenuBuilder;
@@ -14,7 +15,11 @@
 	} = createMenu();
 </script>
 
-<button use:melt={$trigger} class="link menu">Event</button>
+{#if page.url.pathname == "/"}
+	<button class="link menu disabled">Event</button>
+{:else}
+	<button use:melt={$trigger} class="link menu">Event</button>
+{/if}
 <div use:melt={$menu} class="dropdown">
 	<button use:item class="option" onclick={openDialog}>New Event...</button>
 </div>
--- src/lib/component/titlebar/TimelineMenu.svelte
+++ src/lib/component/titlebar/TimelineMenu.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { MenuBuilder } from "./Titlebar.svelte";
 	import { melt } from "@melt-ui/svelte";
+	import { page } from "$app/state";
 
 	interface Props {
 		createMenu: MenuBuilder;
@@ -13,7 +14,11 @@
 	} = createMenu();
 </script>
 
-<button use:melt={$trigger} class="link menu">Timeline</button>
+{#if page.url.pathname == "/"}
+	<button class="link menu disabled">Timeline</button>
+{:else}
+	<button use:melt={$trigger} class="link menu">Timeline</button>
+{/if}
 <div use:melt={$menu} class="dropdown">
 	<button use:item class="option">New Timeline...</button>
 </div>
