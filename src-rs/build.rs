7d69789c638e7cf5261bf2eb53b3ec02d87aad7e
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-10-11 18:20:28 +0100

    chore: build script recreate database

--- src-rs/build.rs
+++ src-rs/build.rs
@@ -3,20 +3,31 @@ use sqlx::{ConnectOptions, Error};
 
 #[tokio::main]
 async fn main() {
-    // Create database for SQLX compile-time validation
+    create_db().await;
+    println!("cargo:rerun-if-changed=migrations");
+    tauri_build::build()
+}
+
+// Create database for SQLX compile-time validation
+async fn create_db() {
     let db_path = "../example.cro";
     let db_url = format!("sqlite://{}", db_path);
 
     println!("cargo:rustc-env=DATABASE_URL={}", db_url);
 
-    let options = SqliteConnectOptions::new()
-        .filename(db_path)
-        .create_if_missing(true);
+    let options = SqliteConnectOptions::new().filename(db_path).create_if_missing(true);
 
-    async { Ok(sqlx::migrate!().run(&mut options.connect().await?).await?) }
-        .await
-        .unwrap_or_else(|e: Error| panic!("Failed to create development database: {}", e));
+    for _ in 0..=1 {
+        if let Err::<(), Error>(e) = async { Ok(sqlx::migrate!().run(&mut options.connect().await?).await?) }.await {
+            println!("cargo::warning=Database migration failed: {}, recreating", e);
 
-    println!("cargo:rerun-if-changed=migrations");
-    tauri_build::build()
+            if let Err(e) = std::fs::remove_file(db_path) {
+                println!("cargo::warning=Failed to remove database file: {}", e);
+            }
+        } else {
+            return;
+        }
+    }
+
+    println!("cargo::error=Failed to create development database after two attempts");
 }
