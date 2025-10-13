7b3bd40f5b711f6114817b35ac30d72e8122d92a
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-10-13 14:09:03 +0100

    refactor: use hooks to display errors as-is

---
+++ src/routes/project/event/[uuid]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<main>
+	<h1>Edit Event</h1>
+</main>
+
+<style lang="scss">
+	main {
+		padding: 10px;
+	}
+</style>
--- src/routes/project/event/[uuid]/+page.ts
+++ src/routes/project/event/[uuid]/+page.ts
@@ -1,9 +1,8 @@
 import { getEvent } from "$lib/bindings";
-import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ params: { uuid } }) => {
 	return {
-		event: await getEvent(uuid).catch((e) => error(404, e)),
+		event: await getEvent(uuid),
 	};
 };
